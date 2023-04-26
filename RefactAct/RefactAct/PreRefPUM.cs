97b933aaf6f6b5a2a2eac5d73a4eaa47546efd56
NaoDekoNeko <ademarx17@gmail.com>
2023-04-26 08:36:25 -0500

    Tec-Refact 1 y 2

---
+++ RefactAct/RefactAct/PreRefPUM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactAct3
+{
+    public class PreRefPUM
+    {
+        public static void Main(string[] args)
+        {
+            Soldier s = new Soldier();
+            Tank t = new Tank();
+            s.GetHealth();
+            t.GetHealth();
+        }       
+    }
+    public class Unit
+    {
+    }
+    class Soldier : Unit
+    {
+        public void GetHealth()
+        {
+            Console.WriteLine("Unit needs health");
+        }
+    }
+    class Tank: Unit
+    {
+        public void GetHealth()
+        {
+            Console.WriteLine("Unit needs health");
+        }
+    }
+}
---
+++ RefactAct/RefactAct/RefPUM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactAct
+{
+    public class RefPUM
+    {
+        public static void Main(string[] args)
+        {
+            Unit s = new Soldier();
+            Unit t = new Tank();
+            s.GetHealth();
+            t.GetHealth();
+        }   
+    }
+    public abstract class Unit
+    {
+        public void GetHealth()
+        {
+            Console.WriteLine("Unit needs health");
+        }
+    }
+    class Soldier : Unit
+    {
+    }
+    class Tank : Unit
+    {
+    }
+}
